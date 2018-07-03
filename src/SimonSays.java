b0df5f4c250586ca79e296891704c2e1c625aeaa
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-03 14:03:59 -0700

    finished SimonSays

--- src/SimonSays.java
+++ src/SimonSays.java
@@ -16,12 +16,17 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+
 import java.io.IOException;
 
 public class SimonSays extends KeyAdapter {
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
+	
+	JFrame frame = new JFrame();
 	
 	HashMap<Integer, String> images = new HashMap<Integer, String>();
 	private int imageIndex;
@@ -31,33 +36,53 @@ public class SimonSays extends KeyAdapter {
 
 	private void makeAlbum() {
 		// 2. Add the four images that match keyboard keys like this: 
-		//images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
-
+		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_LEFT), "left.jpg");
+		images.put(new Integer(KeyEvent.VK_RIGHT), "right.jpg");
+		images.put(new Integer(KeyEvent.VK_DOWN), "down.jpg");
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching key when
 		// 'Simon says' otherwise press a different key"
-		
+		JPanel Panel= new JPanel();
+		JTextPane text= new JTextPane();
+		text.setText("Press the matching key when Simon says otherwise press a different key");
+		Panel.add(text);
+	frame.add(Panel);
+	
 		// 4. Call the showImage method to show an image
+	showImage();
 	}
 
 	public void keyPressed(KeyEvent e) {
 		// 15. Make a points variable to track the score.
-
+		int points = 0;
 		// 16. If the keyCode matches the imageIndex and "Simon says"
+		if(simonSays==true && e.getKeyCode()==imageIndex ) {
+			
 		
 			// 17. Increase the value of score
-		
+		points= points++;
 			// 18. Use the speak method to tell the user they were correct
+		speak("yes you're correct");
+		}
+		else if(e.getKeyCode()!=imageIndex &&  simonSays == false){
+			
 		
 		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't say..."
-		
+		points++;
 			// 20.  Increase the value of score
-		
+		speak("You were correct");
 			// 21. Use the speak method to tell the user they were correct
-		
+		}
 		// 22. Increment tries by 1
-		
+		tries++;
 		// 25. If tries is greater than 9 (or however many you want)...
-		
+		if(tries>3) {
+	System.out.println(points);
+	
+	frame.dispose();
+	showImage();
+	
+		}
 			// 26. Tell the user their score
 		
 			// 27. Exit the program
@@ -67,27 +92,36 @@ public class SimonSays extends KeyAdapter {
 		// 24. Call the showImage method to show a new image
 	}
 
+
 	private void showImage() {
 		// 5. Initialize your frame to a new JFrame()
-		
+		JFrame frame = new JFrame();
 		// 6. Set the frame to visible
-
+frame.setVisible(true);
 		// 7. Uncomment the following line to add a random image to your frame
-		//frame.add(getNextRandomImage());
+		frame.add(getNextRandomImage());
 
 		// 8. Set the name of your frame
-
+frame.setName("Eastern");
 		// 9. Pack the frame
-		
+		frame.pack();
 		// 10. Set the defaultCloseOperation of your from to JFrame.EXIT_ON_CLOSE
-		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 11. Add a key listener to the frame
-
+frame.addKeyListener(this);
 		// 12. Create a new instance of Random
-
+Random Randy = new Random();
 		// 13. Use the Random and the speak method to either say 
 		// "Simon says press this key" or "Press this key"
-
+int Robert= Randy.nextInt(100);
+if (Robert<50) {
+	speak("Press this key");
+	simonSays= false;
+}
+else {
+	speak("Simon says press this key");
+	simonSays = true;
+}
 		// 14. Above, set the value of simonSays to true/false appropriately
 
 		
